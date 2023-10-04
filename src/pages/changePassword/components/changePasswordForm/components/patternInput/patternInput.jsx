682fb328c36ae9af2273fbd6376f3328b8e0070e
nursulton_a <nursulton_a@asakabank.uz>
2023-10-04 10:29:19 +0500

    add changePassworpage add pattern format input

---
+++ src/pages/changePassword/components/changePasswordForm/components/patternInput/patternInput.jsx
@@ -0,0 +1,18 @@
+import { PatternFormat } from 'react-number-format';
+import useTheme from '../../../../../../theme/useTheme';
+import styles from './patternInput.module.scss';
+
+const PatternInput = ({ placeholder, setPhoneNumber }) => {
+  const { darkMode } = useTheme();
+  return (
+    <PatternFormat
+      className={!darkMode ? styles.inputLight : styles.inputDark}
+      placeholder={placeholder}
+      format="+998 (##) ### ## ##"
+      allowEmptyFormatting
+      onChange={(e) => setPhoneNumber(e.target.value)}
+    />
+  );
+};
+
+export default PatternInput;
